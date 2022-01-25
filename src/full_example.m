4dde3b8aa07d974e1d517e0d9ba9450c0ddb69aa
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-24 19:06:03 -0600

    add array and each

--- src/full_example.m
+++ src/full_example.m
@@ -39,3 +39,14 @@ names1 : ['mike', 'tommy']
 names3 : ['mike' 'tommy']
 
 names4: ['mike' 3, "tommy" true]
+
+// each statements
+arr: [1,2,3,4,5,6,7]
+
+addThree = (x) => {
+  add(x 3)
+}
+
+each: arr (item, addThree) => {
+  addThree(item)
+}
