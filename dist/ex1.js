0bd964a7417c26b5dd296ae6dee803d590bac5f4
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-24 19:06:59 -0600

    add

---
+++ dist/ex1.js
@@ -0,0 +1,5 @@
+let job = "programmer";
+let name = "mike";
+let path = 'single';
+// this is a comment and we are going to put an extra line at the bottom for good measure
+console.log(job,name,path);
