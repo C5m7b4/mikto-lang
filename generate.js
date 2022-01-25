d9223f9d18b4a12d013a42a99bf64c0c609c9ba2
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-24 19:06:29 -0600

    created

---
+++ generate.js
@@ -0,0 +1,62 @@
+const fs = require('fs').promises;
+
+async function main() {
+  const filename = process.argv[2];
+  if (!filename) {
+    console.log('please provide a filename');
+    return;
+  }
+
+  const astCode = (await fs.readFile(`ast/${filename}.ast`)).toString();
+  const ast = JSON.parse(astCode);
+  const jsCode = generate(ast);
+  await fs.writeFile(`dist/${filename}.js`, jsCode);
+  console.log(`wrote dist/${filename}.js`);
+}
+
+function generate(ast) {
+  const lines = [];
+  for (let statement of ast) {
+    const line = generateLine(statement);
+    lines.push(line);
+  }
+  return lines.join('\n');
+}
+
+function generateLine(node) {
+  switch (node.type) {
+    case 'comment':
+      return node.value;
+    case 'var_assign':
+      const variable = node.variable.value;
+      const value = generateLine(node.value);
+      return `let ${variable} = ${value};`;
+    case 'function_call':
+      const functionName = node.function_name.value;
+      const arguments =
+        node.arguments && node.arguments.map(generateLine).join(',');
+      if (functionName === 'print') {
+        if (arguments) {
+          return `console.log(${arguments});`;
+        } else {
+          return `console.log();`;
+        }
+      } else {
+        if (arguments) {
+          return `function ${functionName}(${arguments})}`;
+        } else {
+          return `function ${functionName}();`;
+        }
+      }
+    case 'string':
+      return node.value;
+    case 'number':
+      return node.value;
+    case 'identifier':
+      return node.value;
+    default:
+      console.log(`unknown node type detected: ${node.type} $`);
+  }
+}
+
+main().catch((err) => console.log(err.stack));
